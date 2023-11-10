b6e99347351838a8de22eb072e71d712b33c299f
Sivakumar K <105903968+sivakumar7459@users.noreply.github.com>
2023-11-10 11:53:37 +0530

    first commit

---
+++ backup/IKS Allergy Testing_Sample/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6HwVKeURJLA":
+        Script1();
+        break;
+      case "6G9Ew0yyWVg":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById("reset").style.display = "none";
+}
+
+function Script2()
+{
+  document.getElementById("reset").style.display = "none";
+}
+
